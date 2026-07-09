d32da0b4b1f87144a558b947b19cfb72cd169c48
yvsslne48 <amraniyassine196@gmail.com>
2026-07-09 01:35:11 +0100

    chore: mettre à jour l'IP du backend (nouvelle machine)

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.123.128:8000",
+  baseURL: "http://192.168.123.129:8000",
 });
 
 api.interceptors.request.use((config) => {
